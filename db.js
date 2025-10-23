a5454b5a28cd938d932d41c536ca83abb7437b54
St2Day <166264109+St2Day@users.noreply.github.com>
2025-10-22 22:03:18 -0300

    Remove MySQL connection pool setup

--- db.js
+++ db.js
@@ -1,13 +1,1 @@
-const mysql = require("mysql2/promise");
 
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '@Da99083',
-  database: 'sistema_oportunidades',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
-
-module.exports = pool;
