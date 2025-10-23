1795118021b3e6ece1bd25cc49abcde972e58de5
St2Day <166264109+St2Day@users.noreply.github.com>
2025-10-23 18:17:40 -0300

    Change API URL to production server

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000/api';
+const API_URL = 'https://plataforma-oportunidades-backend.onrender.com/api';
 
 // Função para fazer login
 async function fazerLogin(email, senha) {
@@ -109,5 +109,6 @@ function fazerLogout() {
     localStorage.removeItem('token');
     localStorage.removeItem('usuario');
     window.location.href = 'index.html';
+
 
 }
